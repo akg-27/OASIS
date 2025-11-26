eef482e44c13018c88010bc02010620cc0468548
akg-27 <aniketganorkar08@gmail.com>
2025-11-26 23:43:07 +0530

    Created main with routes

---
+++ Backend/app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+# from app.database import init_db
+from app.routers import upload_routes
+from app.routers import visualization_routes
+
+
+app = FastAPI(title="CMLRE Marine Data Platform")
+
+# Initialize DB tables
+# init_db()
+
+# Add Routers
+app.include_router(upload_routes.router)
+app.include_router(visualization_routes.router)
+
+@app.get("/")
+def root():
+    return {"msg": "Backend running..."}
